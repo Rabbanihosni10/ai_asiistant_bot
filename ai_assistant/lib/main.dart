2a8128a421ed32a5522fa5cdd01d53ae26d05345
Rabbanihosni10 <rabbanihosni10@gmail.com~>
2026-01-01 20:06:09 +0600

    Staring With Ai Chatbot

--- ai_assistant/lib/main.dart
+++ ai_assistant/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AI_SahajjoKari',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(),
+      home: Scaffold(
+        body: Text("Hello This is Your Personal AI Chatbot"),
+      ),
     );
   }
 }
